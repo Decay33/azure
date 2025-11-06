9bbeb0eece868ecbdb13418dbaa0e1df0c415993
Decay33 <126214720+Decay33@users.noreply.github.com>
2025-11-06 05:47:16 -0500

    Use COSMOS_DB env var to match Azure config

--- api/src/shared/cosmos.ts
+++ api/src/shared/cosmos.ts
@@ -2,7 +2,7 @@ import { CosmosClient } from "@azure/cosmos";
 
 const endpoint = process.env.COSMOS_ENDPOINT || "";
 const key = process.env.COSMOS_KEY || "";
-const databaseId = process.env.COSMOS_DATABASE || "ysl";
+const databaseId = process.env.COSMOS_DB || "gamehub";
 
 const client = new CosmosClient({ endpoint, key });
 const database = client.database(databaseId);
